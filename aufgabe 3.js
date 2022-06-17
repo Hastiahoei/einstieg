aa8d4bfbedc0e8cad5333e6136dbdb5a8a66a556
Hasti Ahoei <97366164+Hastiahoei@users.noreply.github.com>
2022-06-17 16:13:10 +0200

    Add files via upload

---
+++ aufgabe 3.js
@@ -0,0 +1,45 @@
+
+       const mytext = () => console.log("Hallo Welt");
+       for (let i = 0; i < 10; i++) mytext()
+
+
+
+       const mytext = () => console.log("Willkommen");
+       for(let i=0 ; i < 30 ; i++ ) mytext()
+
+       
+       
+       const mytext = text => console.log("Herzlich willkommen");
+   
+       mytext("ggg");
+       mytext("ffff");
+       mytext("hsdjh");
+       mytext("jdkdkdk");
+       mytext("hjgdjsldj");
+
+
+      const mytext = () => console.log("Yasin");
+       for ( let i= 0 ; i < 7 ; i++) mytext();
+
+
+    
+
+       const hasti = () => console.log("Madar");
+       for (let i = 0 ; i < 7 ; i++);
+       hasti();
+
+      const mytext = () => console.log("Hello Schatz")
+
+      mytext("hello schatz");
+      mytext("hello schatz");
+      mytext("hello schatz");
+      mytext("hello schatz");
+      mytext("hello schatz");
+    
+      
+      let hasti = 0;
+      const mytext = () =>{ console.log(++hasti);
+       }
+      for ( let i = 0; i < 5 ; i++) 
+   
+      mytext();
