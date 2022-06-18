5db7cf5fd9348e4a46fd1ba8557b1d3de801638d
Hasti Ahoei <97366164+Hastiahoei@users.noreply.github.com>
2022-06-18 22:01:09 +0200

    Add files via upload

--- aufgabe 3.js
+++ aufgabe 3.js
@@ -1,5 +1,5 @@
 
-       const mytext = () => console.log("Hallo Welt");
+       /*const mytext = () => console.log("Hallo Welt");
        for (let i = 0; i < 10; i++) mytext()
 
 
@@ -40,6 +40,80 @@
       let hasti = 0;
       const mytext = () =>{ console.log(++hasti);
        }
-      for ( let i = 0; i < 5 ; i++) 
+      for ( let i = 0; i < 5 ; i++) */
    
       mytext();
+
+      function mytext (){
+           for(let i = 0 ; i < 5 ; i++)
+           console.log("hello");
+       }
+       mytext();
+
+
+      function myfunction () {
+           console.log("Hello World")
+       }
+       myfunction();
+       myfunction();
+       myfunction();
+       myfunction();
+       myfunction();
+
+    let countor = 1
+    function myfunction () {
+        for( let i = 0 ; i < 5 ; i++)
+        console.log("Hello World" , countor++) ;
+           
+       }
+
+       myfunction();
+
+     
+     let yourkunde = a * c ;
+     
+     function  myfunction (){
+         console.log( a * c);
+     }
+
+   yourkunde(prompt("zahl1"),prompt("zahl2"));
+
+
+
+   function myfunction ( zahl ){
+      if ( zahl % 3 == 0)
+      console.log("das ist teilbar")
+    else
+    console.log("das ist nicht teilbar")
+      } 
+      myfunction(7);
+
+
+      function myfunction ( zahl1 , zahl2){
+          if (zahl1 % zahl2  ==0)
+          console.log("ja ist es teilbar");
+          else
+          console.log("leider nicht teilbar")
+      }
+      myfunction(12 ,3);
+
+      function myfunction (temp){
+        console.log(temp + 271)
+      }
+      myfunction(20);
+
+
+      let myname  = 'Hallo Welt';
+      for( let i = 1 ; i < myname.length ; i+=2 ){ 
+
+        console.log(myname[i]);
+        }
+        
+
+        let y = "Mein beste freund"
+        let aus = ""
+        for (let i = 0 ; i < y.length ; i+2){
+           aus+=y[i]
+             console.log(aus);
+        }
+       
