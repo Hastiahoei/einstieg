9585c25b0bf0fcb585519264c1891f418c2b226e
Hasti Ahoei <97366164+Hastiahoei@users.noreply.github.com>
2022-06-23 17:56:31 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+let scroll = document.querySelector("button.scroll")
+
+
+window.addEventListener("scroll" , e => {
+    if(window.scrollY > 300 & window.scrollY < 2000){
+       scroll.classList.add("show")
+    } else {
+     scroll.classList.remove("show")
+    }
+})
+
+scroll.addEventListener("click" , e => {
+    if(scroll.classList.contains("show")){ 
+    window.scrollTo({top: 0 , behavior:"smooth"})
+     }
+})
+
+
+let scroll2 = document.querySelector("button.scroll2")
+
+
+window.addEventListener("scroll" , e => {
+    if(window.scrollY > 2000){
+       scroll2.classList.add("show")
+    } else {
+     scroll2.classList.remove("show")
+    }
+})
+
+scroll2.addEventListener("click" , e => {
+    if(scroll.classList.contains("show")){ 
+   document.querySelector("#articels").scrollIntoView({top: 0 , behavior:"smooth"})
+     }
+})
